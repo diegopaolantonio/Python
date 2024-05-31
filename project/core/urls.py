603b68bd4d2e0a4c48a1d4ef461de9c13aaa7a05
diegopaolantonio <dpaolantonio.expertoing@gmail.com>
2024-05-31 20:08:23 -0300

    Commit 5 - Proyecto final - agregue logio, logout y register

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from core.views import index, nosotros
+from core.views import index, nosotros, CustomLoginView, register
 
 
 app_name = "core"
@@ -8,5 +9,8 @@ app_name = "core"
 
 urlpatterns = [
     path("", index, name="index"),
-    path("/nosotros", nosotros, name="nosotros")
+    path("nosotros/", nosotros, name="nosotros"),
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(template_name="core/logout.html"), name="logout"),
+    path("register/", register, name="register"),
 ]
